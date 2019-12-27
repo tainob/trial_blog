a44cfb4886d83833f1bf4e7e80ad8c1c07b3e8f9
tainob <tai@musasaby.co.jp>
2019-12-27 02:42:50 +0000

    add_column_to_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+      added_attrs = [ :username, :email, :password, :password_confirmation　]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
