f37fca23b1950d40bd2214507f98fc8cd92e9c20
tainob <tai@musasaby.co.jp>
2020-01-06 09:07:35 +0000

    add detail pages

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,13 +1,39 @@
 class UserPostsController < ApplicationController
 
+  #投稿一覧ページ（ログイン後ページ）
   def index
     @user_posts = UserPost.order(create_date: :DESC).where(user_id: current_user.id)
   end
 
+  #登録ページ
   def new
     @user_post = UserPost.new
   end
 
+  #編集ページ
+  def edit
+    @user_post = UserPost.find(params[:id])
+  end
+
+  #更新
+  def update
+    @user_post = UserPost.find(params[:id])
+    @user_post.update(
+                      title: params[:user_post][:title],
+                      content: params[:user_post][:content],
+                      category: params[:user_post][:category],
+                      visible: params[:user_post][:visible])
+    redirect_to("/")
+  end
+
+  #削除
+  def destroy
+    @user_post = UserPost.find(params[:id])
+    @user_post.destroy
+    redirect_to("/")
+  end
+
+  #DB書込み
   def create
     @user_post = UserPost.new(user_post_params)
 
@@ -16,7 +42,8 @@ class UserPostsController < ApplicationController
     @user_post.content = params[:user_post][:content]
     @user_post.category = params[:user_post][:category]
 
-    if params[:user_post][:visible] == "1"
+    #非表示設定
+    if params[:user_post][:visible] == "0"
       @user_post.visible = false
     else
       @user_post.visible = true
@@ -24,6 +51,7 @@ class UserPostsController < ApplicationController
 
     @user_post.create_date = DateTime.now
 
+    #公開日
 #    if params[:user_post][:release_date].nil?
 #      @user_post.release_date = DateTime.now
 #    else
@@ -39,11 +67,12 @@ class UserPostsController < ApplicationController
     end
   end
 
+  #他ユーザ投稿一覧ページ
   def list
     @category = params[:category]
     @user_posts = UserPost.order(create_date: :DESC)
                     .where('release_date <= ?', DateTime.now)
-                    .where(visible: true)
+                    .where(visible: false)
                     .where(category: @category)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/users',  to: 'user_posts#index'   #ログイン後
   get '/new'  ,  to: 'user_posts#new'     #ブログを書く
   post '/write', to: 'user_posts#create'  #ブログ保存
+  get  "user_posts/:id/edit"    => 'user_posts#edit'    #ブログ再編集
+  post "user_posts/:id/update"  => "user_posts#update"  #ブログ更新
+  delete "user_posts/:id" => 'user_posts#destroy' #ブログ削除
 
   get '/list',   to: 'user_posts#list'    #他人のブログを表示（カテゴリ別）
 
